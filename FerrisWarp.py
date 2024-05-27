7e64c68aaf51697e13a7486dba7d5785f758fc3d
ExactChangeling <exactchangeling@gmail.com>
2024-05-27 10:39:23 -0500

    Update FerrisWarp.py

--- FerrisWarp.py
+++ FerrisWarp.py
@@ -1,1 +1,22 @@
-print("hello world")
+def use_ids(ids):
+    responsibilities = []
+    for id in ids:
+        if id in data_dict:
+            work_experiences = data_dict[id]['resume']['workExperience']
+            person_responsibilities = '\n'.join([responsibility for experience in work_experiences for responsibility in experience['responsibilities']])
+            responsibilities.append(person_responsibilities)
+    
+    df = pd.DataFrame({'responsibilities': responsibilities})
+
+
+    for index, row in df.iterrows():
+        st.markdown(f"""
+        
+
+            
+{row[0]}
+
+
+        
+
+        """, unsafe_allow_html=True)
